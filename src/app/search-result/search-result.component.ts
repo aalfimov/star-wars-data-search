220d27bc474b2f22e7ceb91efe77330a4204a162
aalfimov <steamcat089@gmail.com>
2019-08-19 09:17:21 +0300

    fix count

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -6,47 +6,70 @@ import {Planets} from '../Interfaces/planets';
 import {Species} from '../Interfaces/species';
 import {Starships} from '../Interfaces/starships';
 import {Vehicles} from '../Interfaces/vehicles';
+import {SwapiAnswer} from "../Interfaces/swapi-answer";
 
 @Component({
-    selector: 'app-search-result',
-    templateUrl: './search-result.component.html',
-    styleUrls: ['./search-result.component.sass']
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.sass']
 })
 export class SearchResultComponent implements OnInit {
-    private countResults: number = null;
-    private filmsResults: Films[];
-    private peopleResults: People[];
-    private planetsResults: Planets[];
-    private speciesResults: Species[];
-    private starshipsResults: Starships[];
-    private vehiclesResults: Vehicles[];
+  get vehiclesResults(): Vehicles[] {
+    return this._vehiclesResults;
+  }
 
-    constructor(private route: ActivatedRoute) {
-    }
+  get starshipsResults(): Starships[] {
+    return this._starshipsResults;
+  }
 
-    ngOnInit() {
-        this.routeDataSubscription();
-    }
+  get speciesResults(): Species[] {
+    return this._speciesResults;
+  }
 
-    private routeDataSubscription() {
-        this.route.data.subscribe(results => {
-            if (results.resultsList) {
-                console.log('count: ' + this.countResults);
-                this.filmsResults = results.resultsList[0].results;
-                this.peopleResults = results.resultsList[1].results;
-                this.planetsResults = results.resultsList[2].results;
-                this.speciesResults = results.resultsList[3].results;
-                this.starshipsResults = results.resultsList[4].results;
-                this.vehiclesResults = results.resultsList[5].results;
-            }
-        });
-    }
+  get planetsResults(): Planets[] {
+    return this._planetsResults;
+  }
 
-    // private checkDataCount(results: any) {
-    //   let count = 0;
-    //   for (let i = 0; results; i++) {
-    //     count = count + results[i].count;
-    //   }
-    //   return count;
-    // }
+  get peopleResults(): People[] {
+    return this._peopleResults;
+  }
+
+  get filmsResults(): Films[] {
+    return this._filmsResults;
+  }
+
+  private countResults: number = null;
+  private _filmsResults: Films[];
+  private _peopleResults: People[];
+  private _planetsResults: Planets[];
+  private _speciesResults: Species[];
+  private _starshipsResults: Starships[];
+  private _vehiclesResults: Vehicles[];
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.routeDataSubscription();
+  }
+
+  private routeDataSubscription() {
+    this.route.data.subscribe(results => {
+      if (results.resultsList) {
+        this.countResults = this.checkDataCount(results.resultsList);
+        this._filmsResults = results.resultsList[0].results;
+        this._peopleResults = results.resultsList[1].results;
+        this._planetsResults = results.resultsList[2].results;
+        this._speciesResults = results.resultsList[3].results;
+        this._starshipsResults = results.resultsList[4].results;
+        this._vehiclesResults = results.resultsList[5].results;
+      }
+    });
+  }
+
+  private checkDataCount(results: SwapiAnswer[]) {
+    let count = 0;
+    results.forEach((obj => count += obj.count));
+    return count;
+  }
 }
